2ab00505d0787fd7e0c7fd60ce425557682a380a
Escuela Digital <ceo@escueladigital.pe>
2016-05-27 23:32:05 -0500

    maquetacion inicial

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,30 @@ const gulp = require('gulp'),
 
 
 gulp.task('sass', () =>
-gulp.src('./dev/scss/styles.scss')
+gulp.src('./dev/styles.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(autoprefixer({
     browsers: ['last 2 versions'],
     cascade: true
   }))
-  .pipe(gulp.dest('./dist/css'))
+  .pipe(gulp.dest('./'))
   .pipe(browserSync.stream())
 );
 
 gulp.task('es6',()=>
-gulp.src('./dev/js/scripts.js')
+gulp.src('./dev/scripts.js')
   .pipe(babel({
     presets: ['es2015']
   }))
-  .pipe(gulp.dest('./dist/js'))
+  .pipe(gulp.dest('./'))
   .on('end', browserSync.reload)
 );
 
 gulp.task('jade', () =>
-gulp.src('./dev/jade/index.jade')
-  .pipe(jade())
+gulp.src('./dev/index.jade')
+  .pipe(jade({
+    pretty: true
+  }))
   .pipe(gulp.dest('./'))
   .on('end', browserSync.reload)
 );
@@ -38,8 +40,8 @@ gulp.task('default', ()=> {
   browserSync.init({
   server: './'
 });
-gulp.watch('./dev/scss/**/*.scss', ['sass']);
-gulp.watch('./dev/js/*.js', ['es6']);
-gulp.watch('./dev/jade/**/*.jade', ['jade']);
+gulp.watch('./dev/**/*.scss', ['sass']);
+gulp.watch('./dev/*.js', ['es6']);
+gulp.watch('./dev/*.jade', ['jade']);
 }
 );
